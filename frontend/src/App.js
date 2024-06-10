4ccbcb5b908da0a461c0aa8f32e74915e045bce0
sina <sinaloptop@gmail.com>
2024-06-10 19:26:50 +0330

    Create Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Heder from './Components/Heder'
+import Footer from './Components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <Heder/>
+      <Container className="py-3">
+        <h1>this is a App Body</h1>
+      </Container>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col>
+            <p className="text-center">Copyright symbol © or the word “copyright”</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
